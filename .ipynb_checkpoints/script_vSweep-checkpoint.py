a8133fa1eb0fb10c3a09a64babea8ff8c5bb8130
Emanuel Casiano-Diaz <Emanuel.Casiano-Diaz@uvm.edu>
2019-06-20 14:20:00 -0400

    Finalized paramater sweep script and script to calculate and merge egs from various files into one

--- .ipynb_checkpoints/script_vSweep-checkpoint.py
+++ .ipynb_checkpoints/script_vSweep-checkpoint.py
@@ -6,15 +6,16 @@ program = 'boseHubbardVMC.py'
 L = '4'
 N = '4'
 U = '9.97631157484440'
-v_min = 1.25
-v_max = 2.001
-v_step = 0.002
+v_min = 1.36
+v_max = 2.0001
+v_step = 0.001
 v_list = np.arange(v_min,v_max,v_step)
-v_list = np.arange(1.25,2.001,0.01)
 M = str(int(1.5E+05))
 t = '1.0'
 mu = '0.0'
 
-for v in v_list:
+print("Completion: ")
+for i,v in enumerate(v_list):
     v = str(v)
-    subprocess.call(['python', program, '--t', t, '--mu', mu, L, N, U, v, M])                
+    subprocess.call(['python', program, '--t', t, '--mu', mu, L, N, U, v, M])  
+    print("%.2f %%"%((i+1)/np.shape(v_list)[0]*100))
